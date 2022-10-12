aa8551881e7d758d62f3ce816e0b5a2ca1b141c7
allgiff <gif19002@byui.edu>
2022-10-11 18:55:06 -0600

    week 5

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,28 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
+
+@Injectable()
 
 export class RecipeService {
     receipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a test', 'https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg'),
-        new Recipe('Another Test Recipe', 'This is a test', 'https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg')
+        new Recipe('A Test Recipe', 'This is a test', 'https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg', [new Ingredient('meat', 1), 
+    new Ingredient('french fries', 20)]),
+        new Recipe('Another Test Recipe', 'This is a test', 'https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg', [new Ingredient('bun', 2), new Ingredient('meat', 1)])
       ];
+
+      constructor(private slService: ShoppingListService) {
+
+      }
 
       getRecipes() {
         return this.recipes.slice();
+      }
+
+      onAddIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
       }
 }
